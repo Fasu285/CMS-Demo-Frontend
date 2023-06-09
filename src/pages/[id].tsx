ef1fe30824427e3159c7b08195e54ec91a975692
Fasil <fasu285@gmail.com>
2023-06-09 19:57:27 +0300

    added back button

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,20 +1,36 @@
 import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from 'next/link'
+
 export default function Page({article} : any) {
     const router = useRouter();
     return (
-      <div>
-        <p>Article ID: {router.query.id}</p>
-        <article key={article.id}>             
-            <h2>{article.attributes.Title}
-            <span class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-              
-                {article.attributes.Category}
-              </span>
-            </h2>
-            
-            </article>   
+      
+<main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+  <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+      <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+          <header className="mb-4 lg:mb-6 not-format">
+<Link href="/"><h4 className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">Go Back</h4></Link>
+              <address className="flex items-center mb-6 not-italic">
+                  <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                  <Image className="mr-4 w-16 h-16 rounded-full" width={500} height={500} src="/image1.jpg" alt="Jese Leos"/>
+                
+                      <div>
+                          <a href="#" rel="author" className="text-xl font-bold text-gray-900 dark:text-white">{ article.attributes.Author}</a>
+                          <p className="text-base font-light text-gray-500 dark:text-gray-400">{article.attributes.Category}</p>
+                          <p className="text-base font-light text-gray-500 dark:text-gray-400"></p>
+                      </div>
+                  </div>
+              </address>
+              <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{ article.attributes.Title}</h1>
+          </header>
+            {article.attributes.Body}
+          <figure>
+              <figcaption>Digital art by Anonymous</figcaption>
+          </figure>
+      </article>
       </div>
-    );
+      </main>);
        
 }
 
